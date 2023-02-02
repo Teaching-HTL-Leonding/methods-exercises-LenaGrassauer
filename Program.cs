4962c46f87eddc72fc47bff5fcf356807af21d17
LenaGrassauer <116261857+LenaGrassauer@users.noreply.github.com>
2023-02-02 10:21:39 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -23,7 +23,11 @@ do
         switch (selection)
         {
             case "0": RunCalculateCircleArea(); break;
-            // TODO: Add additional cases for other methods here
+            case "1": RunRandomInRange(); break;
+            case "2": RunFizzBuzz(); break;
+            case "3": FibonacciByIndex(); break;
+            case "4": AskForNumberInRane(); break;
+            case "5": RunCheckIfPalindrome(); break;
 
             default: break;
         }
@@ -34,7 +38,7 @@ do
 }
 while (selection != "q");
 
-// TODO: Create one code region for each level
+
 #region Calculate Circle Area
 void RunCalculateCircleArea()
 {
@@ -49,3 +53,214 @@ double CalculateCircleArea(double radius)
     return radius * radius * Math.PI;
 }
 #endregion
+
+
+void RunRandomInRange()
+{
+    //Variables 
+    int counter_for_minimum = 0, counter = 0, counter_for_maximum = 0;
+
+    Console.Write("Enter the minimum: ");
+    int minimum = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Enter the maximum:  ");
+    int maximum = int.Parse(Console.ReadLine()!);
+
+    for (int i = 0; i < 1_000_000; i++)
+    {
+        var result = RandomInRange(minimum, maximum);
+
+        if (result == minimum)
+        {
+            counter_for_minimum++;
+        }
+        else if (result == maximum)
+        {
+            counter_for_maximum++;
+        }
+        else
+        {
+            counter++;
+        }
+    }
+    Console.WriteLine($"Counter for {minimum} is {counter_for_minimum} ");
+    Console.WriteLine($"Counter for {maximum} is {counter_for_maximum} ");
+    Console.WriteLine($"Counter for random number is {counter}");
+}
+int RandomInRange(int min, int max)
+{
+    max++;
+    int random = Random.Shared.Next(min, max);
+    return random;
+}
+
+void RunFizzBuzz()
+{
+
+
+
+    System.Console.Write("Please enter a number: ");
+    int num1 = int.Parse(Console.ReadLine()!);
+
+    if (num1 % 3 != 0 && num1 % 5 != 0)
+    {
+        System.Console.WriteLine(num1 + " ");
+    }
+
+    else if (num1 % 3 == 0 && num1 % 5 == 0)
+    {
+        System.Console.WriteLine(" FizzBuzz ");
+    }
+
+    else if (num1 % 3 == 0)
+    {
+        Console.WriteLine(" Fizz ");
+    }
+
+    else if (num1 % 5 == 0)
+    {
+        System.Console.WriteLine(" Buzz ");
+    }
+
+
+
+
+}
+
+void FibonacciByIndex()
+{
+    System.Console.Write("Please enter your first ");
+    int num1 = int.Parse(System.Console.ReadLine()!);
+}
+
+void AskForNumberInRane()
+{
+    System.Console.Write("Please enter your minimum: ");
+    int min = int.Parse(System.Console.ReadLine()!);
+    System.Console.Write("Please enter your maximum: ");
+    int max = int.Parse(System.Console.ReadLine()!);
+
+    System.Console.Write($"Please enter a number between {min} and {max}: ");
+    int num = int.Parse(System.Console.ReadLine()!);
+    if (num < min || num > max) 
+    {
+        do
+        {System.Console.Write("Wrong number, please try again: ");
+        num = int.Parse(System.Console.ReadLine()!);
+        }while (num < min || num > max);   
+         }
+    System.Console.WriteLine("Thank you, your number is valid");
+}
+
+void RunCheckIfPalindrome()
+{
+    Console.Write("Please enter your text: ");
+    string text = Console.ReadLine()!;
+
+    var result = CheckIfPalindrome(text);
+    Console.WriteLine($"The result is: {result}");
+}
+bool CheckIfPalindrome(string input)
+{
+    for (int i = 0; i < input.Length / 2; i++)
+    {
+        if (input[i] != input[input.Length - i - 1])
+        {
+            return false;
+        }
+    }
+    return true;
+
+}
+
+
+
+void RunCheckIfPalindromeAdvanced()
+{
+    Console.Write("Please enter your text: ");
+    string text = Console.ReadLine()!;
+
+    var result = CheckIfPalindromeAdvanced(text);
+    Console.WriteLine($"The result is: {result}");
+}
+
+bool CheckIfPalindromeAdvanced(string input)
+{
+    input = input
+    .Replace(" ", "")
+    .Replace(",", "")
+    .Replace("-", "")
+    .ToLower();
+
+    for (int i = 0; i < input.Length / 2; i++)
+    {
+        if (input[i] != input[input.Length - i - 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+void RunChartBar()
+{
+    Console.Write("Please enter your number: ");
+    double input = double.Parse(Console.ReadLine()!);
+
+    var result = ChartBar(input);
+
+    Console.WriteLine($"The result of {input} is {result}");
+}
+
+string ChartBar(double input)
+{
+    string bar;
+    if (input > 0 && input < 0.1)
+    {
+        bar = "..........";
+    }
+    else if (input >= 0.1 && input < 0.2)
+    {
+        bar = "o.........";
+    }
+    else if (input >= 0.2 && input < 0.3)
+    {
+        bar = "oo........";
+    }
+    else if (input >= 0.3 && input < 0.4)
+    {
+        bar = "ooo.......";
+    }
+    else if (input >= 0.4 && input < 0.5)
+    {
+        bar = "oooo......";
+    }
+    else if (input >= 0.5 && input < 0.6)
+    {
+        bar = "ooooo.....";
+    }
+    else if (input >= 0.6 && input < 0.7)
+    {
+        bar = "oooooo....";
+    }
+    else if (input >= 0.7 && input < 0.8)
+    {
+        bar = "oooooo....";
+    }
+    else if (input >= 0.8 && input < 0.9)
+    {
+        bar = "oooooooo..";
+    }
+    else if (input >= 0.9 && input < 1)
+    {
+        bar = "ooooooooo.";
+    }
+    else
+    {
+        bar = "oooooooooo";
+    }
+    return bar;
+}
+
